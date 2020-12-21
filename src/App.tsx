72602d37d2fddd3dd2ade068986cbbf298981f3e
Zoltan Kovacs <11886025+werdy77@users.noreply.github.com>
2020-12-21 18:47:21 +0100

    feat: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App(): JSX.Element {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactls
-        </a>
-      </header>
+      <div className="m-6 p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <div>
+          <div className="text-xl font-medium text-black">tailwindcss</div>
+          <p className="text-gray-500">It is workin!</p>
+        </div>
+      </div>
     </div>
   )
 }
