002b1d3dda06fbd6f78b773d5222a2117a999557
Zoltan Kovacs <11886025+werdy77@users.noreply.github.com>
2020-12-22 11:25:44 +0100

    feat: resizable playground

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import { SamplePlayground } from './views/SamplePlayground'
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <div className="m-6 p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+    <div className="App flex flex-col h-screen overflow-hidden">
+      <div className="m-6 p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4 flex-grow-0">
         <div>
           <div className="text-xl font-medium text-black">tailwindcss</div>
           <p className="text-gray-500">It is workin!</p>
         </div>
       </div>
-      <SamplePlayground />
+      <SamplePlayground className="flex-grow" />
     </div>
   )
 }
--- src/views/SamplePlayground.tsx
+++ src/views/SamplePlayground.tsx
@@ -1,29 +1,66 @@
-import React from 'react'
+import React, {
+  useState,
+  useMemo,
+  useLayoutEffect,
+  HTMLAttributes,
+} from 'react'
 import { Engine, Scene } from 'react-babylonjs'
 import { Vector3 } from '@babylonjs/core'
 
-const SamplePlayground = (): JSX.Element => (
-  <Engine canvasId="sample-canvas">
-    <Scene>
-      <freeCamera
-        name="camera1"
-        position={new Vector3(0, 5, -10)}
-        target={Vector3.Zero()}
-      />
-      <hemisphericLight
-        name="light1"
-        intensity={0.7}
-        direction={Vector3.Up()}
-      />
-      <sphere
-        name="sphere1"
-        diameter={2}
-        segments={16}
-        position={new Vector3(0, 1, 0)}
-      />
-      <ground name="ground1" width={6} height={6} subdivisions={2} />
-    </Scene>
-  </Engine>
-)
+const SamplePlayground: React.FC<HTMLAttributes<HTMLDivElement>> = ({
+  ...props
+}): JSX.Element => {
+  const [canvasNode, setCanvasNode] = useState<HTMLCanvasElement | undefined>()
+
+  const updateCanvasDimensions = () => {
+    if (canvasNode && canvasNode.parentElement) {
+      canvasNode.width = canvasNode.parentElement.clientWidth
+      canvasNode.height = canvasNode.parentElement.clientHeight
+      canvasNode.style.width = `${canvasNode.width}px`
+      canvasNode.style.height = `${canvasNode.height}px`
+    }
+  }
+
+  useLayoutEffect(() => {
+    window.addEventListener('resize', updateCanvasDimensions)
+    updateCanvasDimensions()
+    return () => {
+      window.removeEventListener('resize', updateCanvasDimensions)
+    }
+  })
+
+  return useMemo(
+    () => (
+      <div {...props}>
+        <Engine canvasId="sample-canvas">
+          <Scene
+            onSceneMount={(e) => {
+              setCanvasNode(e.canvas)
+            }}
+          >
+            <freeCamera
+              name="camera1"
+              position={new Vector3(0, 5, -10)}
+              target={Vector3.Zero()}
+            />
+            <hemisphericLight
+              name="light1"
+              intensity={0.7}
+              direction={Vector3.Up()}
+            />
+            <sphere
+              name="sphere1"
+              diameter={2}
+              segments={16}
+              position={new Vector3(0, 1, 0)}
+            />
+            <ground name="ground1" width={6} height={6} subdivisions={2} />
+          </Scene>
+        </Engine>
+      </div>
+    ),
+    []
+  )
+}
 
 export { SamplePlayground }
