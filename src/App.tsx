6d6df7a5e55c9dce0e2b6dfcbec0b19d55e1a4ee
Tamas Fabi <fabi.tamas.zoltan@gmail.com>
2020-12-23 14:59:57 +0100

    feat: magic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 function App(): JSX.Element {
+  const [isLoading, setIsLoading] = useState(true)
+  useEffect(() => {
+    setTimeout(() => setIsLoading(false), 10000)
+  })
   return (
     <div style={{ display: 'flex', height: '100%' }}>
+      {isLoading && (
+        <div className="frame loading">
+          <div className="loader" />
+        </div>
+      )}
+      <div className="frame vertical" style={{ left: 0 }} />
+      <div className="frame vertical" style={{ right: 0 }} />
+      <div className="frame horizontal" style={{ top: 0 }} />
+      <div className="frame horizontal" style={{ bottom: 0 }}>
+        <div className="bottom">
+          <div>Digital Art Agency - Ervin Hervé-Lóránth</div>
+          <img src="logo.svg" />
+        </div>
+      </div>
       <iframe
         width="100%"
         height="100%"
